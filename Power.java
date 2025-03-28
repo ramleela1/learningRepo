40e124588ab85b327f2e5ddb0b64b6fe8f31d336
ramleela1 <akashreddyqaofficial@gmail.com>
2025-03-28 18:05:46 -0400

    Update Power.java

--- Power.java
+++ Power.java
@@ -3,6 +3,6 @@ public class Power {
 
     @Override
     public String toString() {
-        return super.toString();gghjgjttju
+        return super.toString();gghjgjttjumay be need coffee
     }
 }
